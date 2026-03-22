e4a75b9570c9386071173a3cef4de7621916f84d
Praj003 <prajwols03@gmail.com>
2026-03-22 13:09:46 -0400

    Update2.5

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -8,7 +8,6 @@ function Navbar() {
       <ul className="nav-links">
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
   );
